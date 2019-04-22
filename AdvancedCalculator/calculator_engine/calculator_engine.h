3bb436fab43c71751f74ffb55c65aae9ead8eb08
Louis <louis.groisne@gmail.com>
2019-04-22 14:00:19 +0200

    Support Rational simplification

--- AdvancedCalculator/calculator_engine/calculator_engine.h
+++ AdvancedCalculator/calculator_engine/calculator_engine.h
@@ -8,21 +8,71 @@ public:
 
 	public:
 		explicit ComputableLitteral() {};
-		explicit ComputableLitteral(const double numerator) : numerator(numerator) {}
-		explicit ComputableLitteral(const double numerator, const int denominator): numerator(numerator), denominator(denominator) {}
+		explicit ComputableLitteral(const double numerator) : numerator_(numerator) {}
+		explicit ComputableLitteral(const double numerator, const int denominator): numerator_(numerator), denominator_(denominator) {}
 
-		const double getNumerator() { return numerator; }
-		const double getDenominator() { return denominator; }
+		const double getNumerator() const { return numerator_; }
+		const double getDenominator() const { return denominator_; }
+
+		bool simplify();
 
 	private:
-		const double numerator{0};
-		const int denominator{1};
+		double numerator_{0};
+		int denominator_{1};
 	};
 
-	static ComputableLitteral makeAddition(const ComputableLitteral& rLitteral, const ComputableLitteral& lLitteral) {
-		const int resultDenominator{rLitteral.denominator * lLitteral.denominator};
-		const double resultNumerator{rLitteral.numerator * lLitteral.denominator + lLitteral.numerator * rLitteral.denominator};
-		ComputableLitteral result{resultNumerator, resultDenominator};		
-		return result;
-	}
+	static ComputableLitteral makeAddition(const ComputableLitteral& rLitteral, const ComputableLitteral& lLitteral);
+
+	static bool isInteger(const double value) { return floor(value) == ceil(value); }
+	static int computeGreatestCommonDivisor(const int value1, const int value2);
 };
+
+CalculatorEngine::ComputableLitteral CalculatorEngine::makeAddition(const ComputableLitteral& rLitteral, const ComputableLitteral& lLitteral) {
+	const int resultDenominator{ rLitteral.denominator_ * lLitteral.denominator_ };
+	const double resultNumerator{ rLitteral.numerator_ * lLitteral.denominator_ + lLitteral.numerator_ * rLitteral.denominator_ };
+
+	ComputableLitteral result{ resultNumerator, resultDenominator };
+
+	result.simplify();
+	return result;
+}
+
+int CalculatorEngine::computeGreatestCommonDivisor(const int value1, const int value2) {	
+	if (value1 == value2) {
+		return value1;
+	}
+	else if (value1 == 0)
+	{
+		return value2;
+	}
+	else if (value2 == 0)
+	{
+		return value1;
+	}
+	else {
+		const bool value1IsGreaterThanValue2{ value1 > value2 };
+		const int left{ value1IsGreaterThanValue2 ? (value1 % value2) : (value2 % value1) };
+		
+		if(value1IsGreaterThanValue2)
+			return computeGreatestCommonDivisor(left, value2);
+		else
+			return computeGreatestCommonDivisor(value1, left);
+	}
+}
+
+bool CalculatorEngine::ComputableLitteral::simplify() {
+	if (denominator_ == 1 || !isInteger(numerator_))
+		return false;
+
+	if (numerator_ == 0) {
+		denominator_ = 1;
+		return true;
+	}
+
+	//At this point, numerator_ is known to be an Integer
+	const int greatestCommonDivisor{ computeGreatestCommonDivisor(static_cast<int>(numerator_), denominator_) };
+	numerator_ /= greatestCommonDivisor;
+	denominator_ /= greatestCommonDivisor;
+
+	return greatestCommonDivisor != 1;
+}
--- UnitTestSuite/calculator_engine/calculator_engine_test.cpp
+++ UnitTestSuite/calculator_engine/calculator_engine_test.cpp
@@ -3,11 +3,23 @@
 
 using ComputableLitteral = CalculatorEngine::ComputableLitteral;
 
+ComputableLitteral makeIntegerLitteral(const int value) {
+	return ComputableLitteral(value);
+}
+
+ComputableLitteral makeDecimalLitteral(const double value) {
+	return ComputableLitteral(value);
+}
+
+ComputableLitteral makeRationalLitteral(const int numerator, const int denominator) {
+	return ComputableLitteral(numerator, denominator);
+}
+
 bool areEqual(double lValue, double rValue) {
 	return fabs(lValue - rValue) < DBL_EPSILON;
 }
 
-bool areEqual(ComputableLitteral lLitteral, ComputableLitteral rLitteral)
+bool areEqual(const ComputableLitteral& lLitteral, const ComputableLitteral& rLitteral)
 {
 	return	areEqual(lLitteral.getNumerator(), rLitteral.getNumerator()) && 
 			areEqual(lLitteral.getDenominator(), rLitteral.getDenominator());
@@ -19,34 +31,49 @@ void assertAdditionResult(const double rvalue, const double lvalue, const double
 	EXPECT_TRUE(areEqual(CalculatorEngine::makeAddition(rightLitteral, leftLitteral), ComputableLitteral(result)));
 }
 
-void assertAdditionResult(const ComputableLitteral lLitteral, const ComputableLitteral rLitteral, const ComputableLitteral result) {
+void assertAdditionResult(const ComputableLitteral& lLitteral, const ComputableLitteral& rLitteral, const ComputableLitteral& result) {
 	EXPECT_TRUE(areEqual(CalculatorEngine::makeAddition(lLitteral, rLitteral), result));
 }
 
-TEST(TestCalculatorEngine, TestAdditionsOfSingleValues) {
-	//Integers
+void assertSimplificationResult(ComputableLitteral& litteralToSimplify, const ComputableLitteral& expectedSimplificationResult, const bool shouldBeSimplified) {
+	const bool simplificationWasDone{ litteralToSimplify.simplify() };
+
+	EXPECT_TRUE(areEqual(litteralToSimplify, expectedSimplificationResult));
+	EXPECT_EQ(simplificationWasDone, shouldBeSimplified);
+}
+
+TEST(TestCalculatorEngine, TestAdditionsOfIntegerValues) {
 	assertAdditionResult(0, 0, 0);
 	assertAdditionResult(0, 1, 1);
 	assertAdditionResult(1, 1, 2);
 	assertAdditionResult(10, 10, 20);
-	
-	//Floating points
+}
+
+TEST(TestCalculatorEngine, TestAdditionsOfDecimalValues) {
 	assertAdditionResult(0.5, 0.5, 1);
 	assertAdditionResult(0.6, 1.5, 2.1);
 	assertAdditionResult(0.1561654, 0.1234564, 0.2796218);
-
-	//Rationnal
-	assertAdditionResult(ComputableLitteral(1, 2), ComputableLitteral(0), ComputableLitteral(1, 2));
-}
-
-TEST(TestCalculatorEngine, ) {
-	CalculatorEngine::ComputableLitteral rightLitteral(1, 2);
-	CalculatorEngine::ComputableLitteral leftLitteral(1);
-	EXPECT_TRUE(areEqual(CalculatorEngine::makeAddition(rightLitteral, leftLitteral), ComputableLitteral(3, 2)));
 }
 
 TEST(TestCalculatorEngine, TestAdditionsOfRationnalValues) {
-	CalculatorEngine::ComputableLitteral rightLitteral(1, 2);
-	CalculatorEngine::ComputableLitteral leftLitteral(1, 2);
-	EXPECT_TRUE(areEqual(CalculatorEngine::makeAddition(rightLitteral, leftLitteral), ComputableLitteral(4, 4)));
+	assertAdditionResult(makeRationalLitteral(1, 2), makeIntegerLitteral(0), makeRationalLitteral(1, 2));
+	assertAdditionResult(makeRationalLitteral(1, 2), makeRationalLitteral(1, 2), makeIntegerLitteral(1));
+	assertAdditionResult(makeRationalLitteral(5, 4), makeRationalLitteral(27, 8), makeRationalLitteral(37, 8));
+}
+
+TEST(TestCalculatorEngine, TestSimplificationResult) {
+	assertSimplificationResult(makeRationalLitteral(0, 4), makeIntegerLitteral(0), true);
+	assertSimplificationResult(makeRationalLitteral(2, 2), makeIntegerLitteral(1), true);
+	assertSimplificationResult(makeRationalLitteral(4, 2), makeIntegerLitteral(2), true);
+	assertSimplificationResult(makeRationalLitteral(2, 4), makeRationalLitteral(1, 2), true);
+	assertSimplificationResult(makeRationalLitteral(6, 4), makeRationalLitteral(3, 2), true);
+	assertSimplificationResult(makeRationalLitteral(4, 6), makeRationalLitteral(2, 3), true);
+	assertSimplificationResult(makeRationalLitteral(9, 3), makeRationalLitteral(3, 1), true);
+	assertSimplificationResult(makeRationalLitteral(15, 10), makeRationalLitteral(3, 2), true);
+	
+	assertSimplificationResult(makeRationalLitteral(17, 11), makeRationalLitteral(17, 11), false);
+	assertSimplificationResult(makeRationalLitteral(3, 2), makeRationalLitteral(3, 2), false);
+	assertSimplificationResult(makeRationalLitteral(2, 3), makeRationalLitteral(2, 3), false);
+	assertSimplificationResult(makeRationalLitteral(2, 1), makeIntegerLitteral(2), false);
+	assertSimplificationResult(ComputableLitteral(0.4568, 5), ComputableLitteral(0.4568, 5), false);
 }
